911d0dae8bcc9f73ab3e4ea9e2cf8d3bd8d6cbfe
Ian Carey <ian_carey_936@hotmail.com>
2018-02-23 20:53:05 +0000

    Init

---
+++ MongoConnection.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace MongoDbClient
+{
+    public class MongoConnection : IMongoConnection
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string AuthDatabase { get; set; }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+    }
+}
---
+++ MongoDbConnectionSettingService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using MongoDB.Driver;
+
+namespace MongoDbClient
+{
+    public class MongoDbConnectionSettingService : IMongoDbConnectionSettingService
+    {
+        private MongoClientSettings _mongoClientSettings;
+        private MongoCredential _mongoCredential;
+        private MongoServerAddress _mongoServerAddress;
+
+        public MongoDbConnectionSettingService(MongoConnection connection)
+        {
+            ConfigureClient(connection);
+        }
+
+        public IMongoClient MongoClient { get; private set; }
+
+        private void ConfigureClient(IMongoConnection mongoConfig)
+        {
+            _mongoCredential = MongoCredential.CreateCredential(mongoConfig.AuthDatabase, mongoConfig.Username, mongoConfig.Password);
+            _mongoClientSettings = new MongoClientSettings { Credential = _mongoCredential };
+            _mongoServerAddress = new MongoServerAddress(mongoConfig.Host, mongoConfig.Port);
+            _mongoClientSettings.Server = _mongoServerAddress;
+            MongoClient = new MongoClient(_mongoClientSettings);
+        }
+
+    }
+}
